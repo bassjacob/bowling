19e097ccbd46fdf9b0f5e22418a5c3d5b5ddb697
Jacob Bass <jacob@jacobbass.net>
2019-09-17 07:31:14 +1000

    calculate score of game
    new methods to handle scoring each frame and the whole game

--- src/Bowling/Lib.hs
+++ src/Bowling/Lib.hs
@@ -53,3 +53,47 @@ isGameComplete (Scorecard f1 f2 f3 f4 f5 f6 f7 f8 f9 f10) =
     , (isFrameComplete f9)
     , (isFrameComplete f10)
     ]
+
+getValue :: Score -> Int
+getValue (Pindown i) = i
+getValue (Spare i) = i
+getValue Strike = 10
+getValue Foul = 0
+getValue Pending = 0
+getValue Unused = 0
+
+combine (0, t) _ = (0, t)
+combine (n, t) a = (n - 1, (getValue a) + t)
+
+addScores :: [Score] -> Int
+addScores (Strike:x:y:_) = 10 + getValue x + getValue y
+addScores (_:(Spare _):z:_) = 10 + getValue z
+addScores (x:y:_) = getValue x + getValue y
+
+removeUnused :: [Score] -> [Score]
+removeUnused = filter (\s -> s /= Unused)
+
+getScoresList (NormalFrame s1 s2) = [s1, s2]
+getScoresList (FinalFrame s1 s2 s3) = [s1, s2, s3]
+
+calculateFrame :: Frame -> Frame -> Frame -> Int
+calculateFrame f1 f2 f3 =
+  addScores $ removeUnused $ [f1, f2, f3] >>= getScoresList
+
+
+calculateFrames :: Scorecard -> [Int]
+calculateFrames (Scorecard f1 f2 f3 f4 f5 f6 f7 f8 f9 f10) =
+  [ (calculateFrame f1 f2 f3)
+  , (calculateFrame f2 f3 f4)
+  , (calculateFrame f3 f4 f5)
+  , (calculateFrame f4 f5 f6)
+  , (calculateFrame f5 f6 f7)
+  , (calculateFrame f6 f7 f8)
+  , (calculateFrame f7 f8 f9)
+  , (calculateFrame f8 f9 f10)
+  , (calculateFrame f9 f10 (NormalFrame Unused Unused))
+  , (calculateFrame f10 (NormalFrame Unused Unused) (NormalFrame Unused Unused))
+  ]
+
+calculateScore :: Scorecard -> Int
+calculateScore scorecard = foldr (+) 0 $ calculateFrames scorecard
--- test/Bowling/LibSpec.hs
+++ test/Bowling/LibSpec.hs
@@ -80,3 +80,20 @@ spec = do
       isGameComplete perfectCompleteGame `shouldBe` True
     it "marks an incomplete game as not complete" $ do
       isGameComplete perfectIncompleteGame `shouldBe` False
+  describe "addScores" $ do
+    it "correctly" $ do
+      addScores [Pindown 0, Pindown 0, Pindown 1] `shouldBe` 0
+      addScores [Pindown 1, Spare 9, Pindown 1, Pindown 3] `shouldBe` 11
+      addScores [Strike, Strike, Strike, Pindown 3] `shouldBe` 30
+      addScores [Strike, Strike, Foul, Pindown 3] `shouldBe` 20
+  describe "calculateScore" $ do
+    it "correctly" $ do
+      calculateScore perfectCompleteGame `shouldBe` 300
+      calculateScore simpleScoringGame `shouldBe` 20
+      calculateScore perfectIncompleteGame `shouldBe` 120
+  describe "calculateFrames" $ do
+    it "correctly" $ do
+      calculateFrames perfectCompleteGame `shouldBe` Prelude.replicate 10 30
+      calculateFrames perfectIncompleteGame `shouldBe` (replicate 3 30) ++ [20, 10] ++ (replicate 5 0)
+      calculateFrames unplayedGame `shouldBe` Prelude.replicate 10 0
+      calculateFrames simpleScoringGame `shouldBe` Prelude.replicate 10 2
